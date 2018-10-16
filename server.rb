40cf0b0f742c5c817b6200d51f99bb919e3c544e
Clare Jolly <clarejollyuk@gmail.com>
2018-10-16 11:32:00 +0100

    app working - needs some formatting improvements, but works

--- server.rb
+++ server.rb
@@ -40,42 +40,35 @@ socket.puts "==============================\n"
 socket.puts "What do you want to do? (add, view, view body)"
 do_next = socket.gets.chomp
 notebook = Notebook.new
+
 while do_next != "quit"
-
-if do_next.downcase == "add"
-
-socket.puts "Enter note title"
-notetitle = socket.gets.chomp
-socket.puts "Enter note body"
-notebody = socket.gets.chomp
-
-
-# socket.puts "You said: #{they_said}."
-# they_said = socket.gets.chomp
-
-
-note = Note.new(notebook, notetitle, notebody)
-socket.puts note
-socket.puts "What do you want to do?"
-do_next = socket.gets.chomp
-# socket.puts "Enter note"
-# notetitle = socket.gets.chomp
-
-
-elsif do_next.downcase == "view"
-  socket.puts notebook.note_titles
-  socket.puts "What do you want to do?"
-  do_next = socket.gets.chomp
-elsif do_next.downcase == "view body"
-  socket.puts "which note?"
-  notetitle = socket.gets.chomp
-  socket.puts notebook.note_body(notetitle)
-  socket.puts "What do you want to do?"
-  do_next = socket.gets.chomp
-else
-  socket.puts "Sorry, what? What do you want to do?"
-  do_next = socket.gets.chomp
-end
+  if do_next.downcase == "add"
+    socket.puts "Enter note title"
+    notetitle = socket.gets.chomp
+    socket.puts "Enter note body"
+    notebody = socket.gets.chomp
+    note = Note.new(notebook, notetitle, notebody)
+    socket.puts "Note added"
+    socket.puts "\nWhat do you want to do?\n"
+    do_next = socket.gets.chomp
+  elsif do_next.downcase == "view"
+    socket.puts "HERE ARE THE NOTES"
+    socket.puts "==================\n"
+    socket.puts notebook.note_titles
+    socket.puts "\nWhat do you want to do?\n"
+    do_next = socket.gets.chomp
+  elsif do_next.downcase == "view body"
+    socket.puts "which note?"
+    notetitle = socket.gets.chomp
+    socket.puts "HERE IS THE BODY OF #{notetitle.upcase}"
+    socket.puts "====================================\n"
+    socket.puts notebook.note_body(notetitle)
+    socket.puts "\nWhat do you want to do?\n"
+    do_next = socket.gets.chomp
+  else
+    socket.puts "Sorry, what? What do you want to do?"
+    do_next = socket.gets.chomp
+  end
 end
 socket.puts "You said: . Goodbye!!"
 socket.close
